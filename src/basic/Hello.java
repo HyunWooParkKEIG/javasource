0314391891271b0386b199b4b115558adddc13e6
hyeonwoopark <yey6687@daum.net>
2021-04-12 15:17:02 +0900

    Hello updated

--- src/basic/Hello.java
+++ src/basic/Hello.java
@@ -3,7 +3,9 @@ package basic;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");	
+		System.out.println("Hello");
+		System.out.println("안녕하세요.");
+		System.out.println("반갑습니다.");
 	}
 
 }
