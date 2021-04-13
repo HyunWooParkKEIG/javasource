5feda0d81dc908d5b60c0651fb30c3b003dd7b5b
hyeonwoopark <yey6687@daum.net>
2021-04-13 10:44:24 +0900

    ch14 Car TV Product created

---
+++ src/ch14/Car.java
@@ -0,0 +1,20 @@
+package ch14;
+
+public class Car {
+	private String company;
+
+	public Car(String company) {
+		super();
+		this.company = company;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	
+}
